18e08ef15d9730511a1420d1e332d0fb0a1aa048
mr_maroga <45645052+AdvocateM@users.noreply.github.com>
2023-07-11 17:03:20 +0200

    Update app.js

--- app.js
+++ app.js
@@ -101,7 +101,7 @@ function dom(data){
 
     const footer = `<div class="attribution">
     Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
-    Coded by <a href="#">Your Name Here</a>.
+    Coded by <a href="https://mrmaroga.co.za">Advocate</a>.
     </div>`
   columns.innerHTML = columns.innerHTML + footer;
 }
